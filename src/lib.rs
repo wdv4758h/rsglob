a038d57912cb87ccea9630ee30de581edcaafbcd
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2018-06-03 22:40:16 +0800

    Remove functions that are not in glob public API

--- src/lib.rs
+++ src/lib.rs
@@ -21,18 +21,11 @@ pub fn register_constants(py: Python, m: &PyModule) -> PyResult<()> {
 fn init(py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "escape")]
     pub fn escape(py: Python) -> PyResult<PyObject> { Ok(py.None()) }
-    #[pyfn(m, "glob0")]
-    pub fn glob0(py: Python) -> PyResult<PyObject> { Ok(py.None()) }
 
     #[pyfn(m, "iglob")]
     pub fn iglob(py: Python, pathname: String) -> PyResult<PyObject> {
         glob_str(py, pathname)
     }
-
-    #[pyfn(m, "fnmatch")]
-    pub fn fnmatch(py: Python) -> PyResult<PyObject> { Ok(py.None()) }
-    #[pyfn(m, "glob1")]
-    pub fn glob1(py: Python) -> PyResult<PyObject> { Ok(py.None()) }
 
     #[pyfn(m, "glob_str")]
     pub fn glob_str(py: Python, pathname: String) -> PyResult<PyObject> {
@@ -57,9 +50,6 @@ fn init(py: Python, m: &PyModule) -> PyResult<()> {
             .collect();
         Ok(result.to_object(py))
     }
-
-    #[pyfn(m, "has_magic")]
-    pub fn has_magic(py: Python) -> PyResult<PyObject> { Ok(py.None()) }
 
     Ok(())
 }
