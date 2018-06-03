8b61d791f7fa69c32cb222a57bcb7859bc0c7f57
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2018-06-03 22:37:59 +0800

    Add workaround for iglob

--- rsglob/__init__.py
+++ rsglob/__init__.py
@@ -1,3 +1,7 @@
+"""Filename globbing utility."""
+
+__all__ = ["glob", "iglob", "escape"]
+
 VERSION = '0.0.1'
 
 try:
@@ -9,6 +13,13 @@ try:
         elif isinstance(pathname, bytes):
             # FIXME: better way ?
             return list(map(str.encode, _glob.glob_str(pathname.decode())))
+
+    def iglob(pathname, *, recursive=False):
+        # FIXME: better way ?
+        return iter(glob(pathname, recursive=recursive))
+
+    def escape(pathname):
+        pass
 
 except:
     print("no _rsglob exist")
